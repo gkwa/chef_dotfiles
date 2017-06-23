0cccc3d4727dc5923cf7e50eb0b131c0389ee910
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-23 00:53:03 -0700

    Overwrite pre-existing files with force checkout

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,13 +1,11 @@
 package 'git'
 
-file '/root/.bashrc' do
-  action :delete
-end
-
-file '/root/.profile' do
-  action :delete
-end
-
-git '/root' do
-  repository 'https://github.com/taylormonacelli/dotfiles.git'
+execute 'git checkout' do
+  cwd ENV['HOME']
+  command <<-EOH
+    git init
+    git remote add origin https://github.com/taylormonacelli/dotfiles.git
+    git fetch --depth 50
+    git checkout --force origin/master
+  EOH
 end

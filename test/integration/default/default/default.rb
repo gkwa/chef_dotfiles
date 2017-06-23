3fed158b7639042d7d81af53fb75831a0bdc6b02
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-23 14:40:43 -0700

    [skip travis] fix windows test

--- test/integration/default/default/default.rb
+++ test/integration/default/default/default.rb
@@ -1,10 +1,11 @@
 if os[:family] == 'windows'
 
-  describe file('c:\Users\appveyor\.tmux.conf') do
+  describe file('c:\.tmux.conf') do
     it { should be_a_file }
   end
 
 else
+
   describe file('/root/.tmux.conf') do
     it { should be_a_file }
   end

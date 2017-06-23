e25dfb45b6027c770ea95821f78603d3bfeb26ec
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-23 09:24:29 -0700

    Pacify foodcritic

--- metadata.rb
+++ metadata.rb
@@ -6,15 +6,8 @@ description 'Installs/Configures chef_dotfiles'
 long_description 'Installs/Configures chef_dotfiles'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/chef_dotfiles/issues'
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/chef_dotfiles'
+supports 'fedora'
+supports 'ubuntu'
+supports 'windows'
+issues_url 'https://github.com/taylormonacelli/chef_dotfiles/issues'
+source_url 'https://github.com/taylormonacelli/chef_dotfiles'

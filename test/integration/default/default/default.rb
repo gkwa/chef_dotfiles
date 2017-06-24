40269a868596b6262efcd6d0806491d81fd8318d
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-24 00:53:57 -0700

    Use git resource instead of bash or powershell

--- test/integration/default/default/default.rb
+++ test/integration/default/default/default.rb
@@ -1,9 +1,9 @@
 if os[:family] == 'windows'
-  describe file('c:\Users\vagrant\.tmux.conf') do
+  describe file('c:\Users\vagrant\dotfiles\.tmux.conf') do
     it { should be_a_file }
   end
 else
-  describe file('/root/.tmux.conf') do
+  describe file('/root/dotfiles/.tmux.conf') do
     it { should be_a_file }
   end
 end

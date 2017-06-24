6c1c0c3187a2123c821f76b072a619e2155545f9
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-24 11:55:40 -0700

    'chef install' wont work with this

--- Policyfile.rb
+++ Policyfile.rb
@@ -2,5 +2,4 @@ name 'chef_dotfiles'
 default_source :supermarket
 run_list 'chef_dotfiles::default'
 named_run_list :run, 'chef_dotfiles::default'
-named_run_list :test, 'test::default'
 cookbook 'chef_dotfiles', path: '.', branch: 'master'

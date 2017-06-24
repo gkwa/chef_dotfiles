2f0099613c139a65271a51569293b0c381140071
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-24 00:45:05 -0700

    Remove default from Policyfile
    Whitespace

--- Policyfile.rb
+++ Policyfile.rb
@@ -3,5 +3,3 @@ default_source :supermarket
 run_list 'chef_dotfiles::default'
 named_run_list :run, 'chef_dotfiles::default'
 cookbook 'chef_dotfiles', path: '.', branch: 'master'
-
-default['chef_dotfiles']['dotfiles_install_path'] = 'c:\Users\Vagrant'

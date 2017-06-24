8a433e902f4b47b25f215bc837a4576b48e5a2a7
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-24 11:32:40 -0700

    Try get chef-client to run test::default

--- Policyfile.rb
+++ Policyfile.rb
@@ -2,4 +2,5 @@ name 'chef_dotfiles'
 default_source :supermarket
 run_list 'chef_dotfiles::default'
 named_run_list :run, 'chef_dotfiles::default'
+named_run_list :test, 'test::default'
 cookbook 'chef_dotfiles', path: '.', branch: 'master'

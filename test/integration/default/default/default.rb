6ef40ae057589a881be371bb322ac1b9392f1bd6
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-23 23:28:08 -0700

    whitespace

--- test/integration/default/default/default.rb
+++ test/integration/default/default/default.rb
@@ -1,13 +1,9 @@
 if os[:family] == 'windows'
-
   describe file('c:\Users\vagrant\.tmux.conf') do
     it { should be_a_file }
   end
-
 else
-
   describe file('/root/.tmux.conf') do
     it { should be_a_file }
   end
-
 end

2bc3c5a48085f1eeefd4ed7f802d252ce3d265a0
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-23 18:00:28 -0700

    Add policyfile to shorten test time

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,7 +1,7 @@
 name 'chef_dotfiles'
 default_source :supermarket
 run_list 'chef_dotfiles::default'
-named_run_list :test, 'chef_dotfiles::default'
+named_run_list :run, 'chef_dotfiles::default'
 cookbook 'chef_dotfiles', path: '.', branch: 'master'
 
 default['chef_dotfiles']['dotfiles_install_path'] = 'c:\Users\Vagrant'

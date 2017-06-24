d294205c14d79e7598f16ff6e518ce21a8d8c3a1
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-24 01:09:43 -0700

    FC002: surprising we don't need to quote node[]

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,4 @@
-git "#{node['chef_dotfiles']['dotfiles_install_path']}" do
+git node['chef_dotfiles']['dotfiles_install_path'] do
   repository 'https://github.com/taylormonacelli/dotfiles.git'
   revision 'master'
   depth 50

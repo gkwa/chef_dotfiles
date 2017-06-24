22fe7c41a5f0d9809134c701a5794bcedf4990e2
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-24 01:17:41 -0700

    Revert "Rely on chef for git app instead of installing it"
    This reverts commit bced8cad635a357d18362795add8edbcdb125c12.
    
    # Conflicts:
    #	recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,11 @@
+include_recipe 'chef-sugar::default'
+
+if windows?
+  chocolatey_package 'git'
+else
+  package 'git'
+end
+
 git node['chef_dotfiles']['dotfiles_install_path'] do
   repository 'https://github.com/taylormonacelli/dotfiles.git'
   revision 'master'

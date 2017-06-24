bced8cad635a357d18362795add8edbcdb125c12
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-24 00:53:57 -0700

    Rely on chef for git app instead of installing it
    Use git resource instead of bash or powershell

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,5 @@
 include_recipe 'chef-sugar::default'
 
-if windows?
-  chocolatey_package 'git'
-else
-  package 'git'
-end
-
 git "#{node['chef_dotfiles']['dotfiles_install_path']}" do
   repository 'https://github.com/taylormonacelli/dotfiles.git'
   revision 'master'

f29074be542de53c11696a2498658cd4f065b76e
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-23 17:19:33 -0700

    Log current directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,15 @@ end
 
 execute 'git checkout' do
   cwd node['dotfiles_install_path']
+
+  cmd = <<-EOH
+  pwd
+  EOH
+
+  log = Mixlib::ShellOut.new(cmd)
+  log.run_command
+  Chef::Log.error "\n\n" + '=' * 80 + "\n\ndocker status: #{log.stdout}\n\n" + '=' * 80
+
   command <<-EOH
     git init
     git remote add origin https://github.com/taylormonacelli/dotfiles.git

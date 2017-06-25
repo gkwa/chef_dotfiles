12c3106b980f83c11bae99fdc571943db6731c11
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-24 19:40:18 -0700

    On windows move to Documents folder

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,6 @@
-default['chef_dotfiles']['dotfiles_path'] = "#{ENV['HOME']}/dotfiles"
+default['chef_dotfiles']['dotfiles_path'] =
+  if node['platform'] == 'windows'
+    "#{ENV['HOME']}/Documents/dotfiles"
+  else
+    "#{ENV['HOME']}/dotfiles"
+  end

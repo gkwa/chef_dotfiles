d3f4b40350c8edf1263387d90fb894ca15ca6574
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-25 07:41:51 -0700

    Attributes in .kitchen.*.yml

--- test/integration/default/default/default.rb
+++ test/integration/default/default/default.rb
@@ -1,5 +1,5 @@
 if os[:family] == 'windows'
-  describe file('c:\Users\vagrant\AppData\Local\dotfiles\.tmux.conf') do
+  describe file('c:\Users\vagrant\dotfiles\.tmux.conf') do
     it { should be_a_file }
   end
 else

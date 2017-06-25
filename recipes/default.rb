ecd5541e3524bb01387d4e48469cf536a25b4d8e
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-25 12:32:38 -0700

    git checkut full repo

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,8 +13,6 @@ end
 
 git "#{node['chef_dotfiles']['dotfiles_path']}/.git" do
   repository 'https://github.com/taylormonacelli/dotfiles.git'
-  revision 'master'
-  depth 50
   action :sync
 end
 

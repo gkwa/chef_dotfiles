d5a7d59767949b03b8af9eb87acb0ce6b1a8c437
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-24 20:20:24 -0700

    Start learning $XDG spec ($XDG_CONFIG_HOME)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['chef_dotfiles']['dotfiles_path'] =
   if node['platform'] == 'windows'
-    "#{ENV['HOME']}/Documents/dotfiles"
+    "#{ENV['LOCALAPPDATA']}/dotfiles"
   else
     "#{ENV['HOME']}/dotfiles"
   end

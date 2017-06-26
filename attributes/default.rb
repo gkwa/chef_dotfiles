bab7cd66fd77800b12cf01a3c01b86f8f8493efd
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-25 21:45:35 -0700

    Windows path

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['chef_dotfiles']['dotfiles_path'] =
   if node['platform'] == 'windows'
-    "#{ENV['LOCALAPPDATA']}/dotfiles"
+    "#{ENV['LOCALAPPDATA']}\\dotfiles"
   else
     "#{ENV['HOME']}/.config"
   end

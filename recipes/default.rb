ffc5d4729585678d850b1a3b2157c6cc37fc0065
Taylor Monacelli <taylormonacelli@gmail.com>
2017-06-25 21:41:23 -0700

    Revert "Remove try/catch"
    This reverts commit 1fd258e2034bb9a2f84ab5d13cad8cbe9b49713f.

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,7 +13,10 @@ if node['platform'] == 'windows'
       git init
       git remote add origin https://github.com/taylormonacelli/dotfiles.git
       git fetch --depth 50
-      git checkout --force master
+      try {
+        git checkout --force master
+      } catch {
+      }
     EOH
     not_if 'test-path .git'
   end

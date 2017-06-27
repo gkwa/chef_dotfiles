66d2a13d3a280c9a29ca3e3c6a014d9f751d706c
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-06-27 06:05:33 -0700

    WIP: test on macosx in travis
    Copy setup from https://github.com/dhoer/chef-chrome/tree/macosx
    
    yada-yada

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license 'All Rights Reserved'
 description 'Installs/Configures chef_dotfiles'
 long_description 'Installs/Configures chef_dotfiles'
 version '0.1.1'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.14' if respond_to?(:chef_version)
+
 supports 'fedora'
 supports 'ubuntu'
 supports 'windows'

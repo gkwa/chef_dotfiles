cc80272d2ec57146518c56e48e156f1d8414ea7f
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-06-27 20:59:21 -0700

    Try all over again

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures chef_dotfiles'
 long_description 'Installs/Configures chef_dotfiles'
 version '0.1.1'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 12.14'
 
 supports 'fedora'
 supports 'ubuntu'

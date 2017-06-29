1b2f1790972fbc63dffca2df2eb2aa5fd3b7d983
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-06-29 16:48:57 -0700

    Fix expected 'File /root/.config/.tmux.conf.file?'

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['chef_dotfiles']['dotfiles_path'] =
-  if node['platform'] == 'windows'
-    "#{ENV['LOCALAPPDATA']}\\dotfiles"
-  else
+  if node['platform'] != 'windows'
     "#{ENV['HOME']}/.config"
+  else
+    "#{ENV['LOCALAPPDATA']}\\dotfiles"
   end

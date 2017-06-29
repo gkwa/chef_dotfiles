1cb910c6beb89a4e8032314ab83741e52bb5eb8d
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-06-29 16:48:57 -0700

    Fix expected 'File /root/.config/.tmux.conf.file?'

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,12 +4,12 @@ if os[:family] == 'windows'
   describe file('c:\Users\vagrant\.tmux.conf') do
     it { should be_a_file }
   end
-elsif os[:family] == 'darwin'
-  describe file('/Users/travis/.config/.tmux.conf') do
+elsif os[:family] == 'macOS'
+  describe file('/Users/travis.tmux.conf') do
     it { should be_a_file }
   end
 else
-  describe file('/root/.config/.tmux.conf') do
+  describe file('/root.tmux.conf') do
     it { should be_a_file }
   end
 end

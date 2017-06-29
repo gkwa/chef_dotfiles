62ba72aebfb28045a1b948c091e3564e8d07cecd
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-06-28 20:08:39 -0700

    Proally delete this

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license 'All Rights Reserved'
 description 'Installs/Configures chef_dotfiles'
 long_description 'Installs/Configures chef_dotfiles'
 version '0.1.1'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.14'
+
 supports 'fedora'
 supports 'ubuntu'
 supports 'windows'
@@ -16,3 +17,5 @@ source_url 'https://github.com/taylormonacelli/chef_dotfiles'
 license 'Apache-2.0'
 depends 'windows'
 depends 'chocolatey'
+
+depends 'dmg', '>= 3.0'

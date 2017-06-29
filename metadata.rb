4977c1f705e11be3e60ccd8194805ee08f752b72
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-06-28 19:37:25 -0700

    Start over: revert to 63471f9

--- metadata.rb
+++ metadata.rb
@@ -5,17 +5,13 @@ license 'All Rights Reserved'
 description 'Installs/Configures chef_dotfiles'
 long_description 'Installs/Configures chef_dotfiles'
 version '0.1.1'
-chef_version '>= 12.14'
-
+chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'fedora'
 supports 'ubuntu'
 supports 'windows'
-supports 'mac_os_x'
 issues_url 'https://github.com/taylormonacelli/chef_dotfiles/issues'
 source_url 'https://github.com/taylormonacelli/chef_dotfiles'
 
 license 'Apache-2.0'
 depends 'windows'
 depends 'chocolatey'
-
-depends 'dmg', '>= 3.0'

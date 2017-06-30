d436157635b88a320e1133e82c07a6d1501bf2c6
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-06-29 17:32:49 -0700

    Pull diff from macosx branch

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,6 @@
 default['chef_dotfiles']['dotfiles_path'] =
-  if node['platform'] == 'windows'
-    "#{ENV['LOCALAPPDATA']}\\dotfiles"
-  elsif node['platform'] == 'macOS'
-    "#{ENV['LOCALAPPDATA']}/.config"
+  if node['platform'] != 'windows'
+    "#{ENV['HOME']}/.config"
   else
-    "/root/.config"
+    "#{ENV['LOCALAPPDATA']}\\dotfiles"
   end

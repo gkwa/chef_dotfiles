841343e7ccd774cefed22c90abda84ae48d6a52b
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-02 10:01:36 -0700

    Revert "I like when/case better than if/conditionals"
    This reverts commit 4aba1de5a4e476d41b20ba42719fa1a6a75b6ae7.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,6 @@
 default['chef_dotfiles']['dotfiles_path'] =
-case node['platform']
-when 'windows'
-  "#{ENV['LOCALAPPDATA']}\\dotfiles"
-else
-  "#{ENV['HOME']}/.config"
-end
+  if node['platform'] == 'windows'
+    "#{ENV['LOCALAPPDATA']}\\dotfiles"
+  else
+    "#{ENV['HOME']}/.config"
+  end

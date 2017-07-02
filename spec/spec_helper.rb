8689c13bf25be1011429e9fbf7221fa7835a8717
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-01 19:45:54 -0700

    Fix `require': cannot load such file -- spec_helper (LoadError)
    Full log
    https://travis-ci.org/TaylorMonacelli/chef_dotfiles/jobs/249234798
    
    ...
    Installing serverspec 1.16.0
    Bundle complete! 1 Gemfile dependency, 11 gems now installed.
    Use `bundle info [gemname]` to see where a bundled gem is installed.
    /opt/chef/embedded/bin/ruby -I/opt/verifier/suites/serverspec -I/opt/verifier/gems/gems/rspec-support-3.6.0/lib:/opt/verifier/gems/gems/rspec-core-3.6.0/lib /opt/chef/embedded/bin/rspec --pattern /opt/verifier/suites/serverspec/\*\*/\*_spec.rb --color --format documentation --default-path /opt/verifier/suites/serverspec
    /opt/chef/embedded/lib/ruby/2.4.0/rubygems/core_ext/kernel_require.rb:55:in `require': cannot load such file -- spec_helper (LoadError)
    	from /opt/chef/embedded/lib/ruby/2.4.0/rubygems/core_ext/kernel_require.rb:55:in `require'
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/configuration.rb:1453:in `block in requires='
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/configuration.rb:1453:in `each'
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/configuration.rb:1453:in `requires='
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/configuration_options.rb:112:in `block in process_options_into'
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/configuration_options.rb:111:in `each'
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/configuration_options.rb:111:in `process_options_into'
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/configuration_options.rb:21:in `configure'
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/runner.rb:99:in `setup'
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/runner.rb:86:in `run'
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/runner.rb:71:in `run'
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/lib/rspec/core/runner.rb:45:in `invoke'
    	from /opt/verifier/gems/gems/rspec-core-3.6.0/exe/rspec:4:in `<top (required)>'
    	from /opt/chef/embedded/bin/rspec:22:in `load'
    	from /opt/chef/embedded/bin/rspec:22:in `<main>'
    /opt/chef/embedded/bin/ruby -I/opt/verifier/suites/serverspec -I/opt/verifier/gems/gems/rspec-support-3.6.0/lib:/opt/verifier/gems/gems/rspec-core-3.6.0/lib /opt/chef/embedded/bin/rspec --pattern /opt/verifier/suites/serverspec/\*\*/\*_spec.rb --color --format documentation --default-path /opt/verifier/suites/serverspec failed
    !!!!!! Ruby Script [/opt/verifier/gems/gems/busser-serverspec-0.5.10/lib/busser/runner_plugin/../serverspec/runner.rb /opt/verifier/suites/serverspec] exit code was 1
    >>>>>> ------Exception-------
    >>>>>> Class: Kitchen::ActionFailed
    >>>>>> Message: 1 actions failed.
    >>>>>>     Verify failed on instance <default-centos-7>.  Please see .kitchen/logs/default-centos-7.log for more details
    >>>>>> ----------------------
    >>>>>> Please see .kitchen/logs/kitchen.log for more details
    >>>>>> Also try running `kitchen diagnose --all` for configuration
    The command "KITCHEN_LOCAL_YAML=.kitchen.dokken.yml /opt/chefdk/embedded/bin/kitchen verify ${INSTANCE}" exited with 20.
    Done. Your build exited with 1.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'chefspec/berkshelf'
 RSpec.configure do |config|
   config.color = true               # Use color in STDOUT
   config.formatter = :documentation # Use the specified formatter
-  config.log_level = :error         # Avoid deprecation notice SPAM
+  config.log_level = :debug         # Avoid deprecation notice SPAM
 
   # run all specs when using a filter, but no spec match
   config.run_all_when_everything_filtered = true

30b1c79c682647c7a7f4b75a38bc7567e89fb6f9
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-02 10:27:26 -0700

    rubocop: indent multi line case

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['chef_dotfiles']['dotfiles_path'] =
-case node['platform']
-when 'windows'
-  "#{ENV['LOCALAPPDATA']}\\dotfiles"
-else
-  "#{ENV['HOME']}/.config"
-end
+  case node['platform']
+  when 'windows'
+    "#{ENV['LOCALAPPDATA']}\\dotfiles"
+  else
+    "#{ENV['HOME']}/.config"
+  end

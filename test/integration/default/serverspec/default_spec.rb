ef80749a52b562740c3e57322dc11f136cfb7634
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-01 21:40:35 -0700

    Revert "Fix `require': cannot load such file -- spec_helper (LoadError)"
    This reverts commit 228d04611014886f46c7ded4395f1ea0596b6d92.

---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,15 @@
+require 'serverspec_helper'
+
+if os[:family] == 'windows'
+  describe file('c:\Users\vagrant\.tmux.conf') do
+    it { should be_a_file }
+  end
+elsif os[:family] == 'macOS'
+  describe file('/Users/travis/.tmux.conf') do
+    it { should be_a_file }
+  end
+else
+  describe file('/root/.tmux.conf') do
+    it { should be_a_file }
+  end
+end

58baeaba88e6e41f344807a4d64623eb266f8697
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-02 23:26:56 -0700

    reorg metadata

--- metadata.rb
+++ metadata.rb
@@ -6,13 +6,19 @@ description 'Installs/Configures chef_dotfiles'
 long_description 'Installs/Configures chef_dotfiles'
 version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+
+issues_url 'https://github.com/taylormonacelli/chef_dotfiles/issues'
+source_url 'https://github.com/taylormonacelli/chef_dotfiles'
+
 supports 'fedora'
 supports 'ubuntu'
 supports 'windows'
 supports 'mac_os_x'
+
 issues_url 'https://github.com/taylormonacelli/chef_dotfiles/issues'
 source_url 'https://github.com/taylormonacelli/chef_dotfiles'
 
 license 'Apache-2.0'
 depends 'windows'
 depends 'chocolatey'
+

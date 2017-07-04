d056afe9d3e47183c1540237d8b0e36f7919814a
Taylor Monacelli <taylormonacelli@gmail.com>
2017-07-04 11:54:06 -0700

    $XDG_CONFIG_HOME defaults to %LOCALAPPDATA% on windows

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['chef_dotfiles']['dotfiles_path'] =
   case node['platform']
   when 'windows'
-    "#{ENV['LOCALAPPDATA']}\\dotfiles"
+    "#{ENV['LOCALAPPDATA']}"
   else
     "#{ENV['HOME']}/.config"
   end
